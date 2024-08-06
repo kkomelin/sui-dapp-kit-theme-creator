723bff8db50c9ef0e120f11536d3ac0ed3309410
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-08-06 22:37:36 +0200

    Added missing types for env variables.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_APP_NAME: string
   readonly VITE_APP_DESCRIPTION: string
+  readonly VITE_APP_URL: string
+  readonly VITE_THEME_INSTALLATION_GUIDE: string
 }
 
 interface ImportMeta {
